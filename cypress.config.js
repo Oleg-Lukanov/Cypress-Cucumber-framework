640aa3b9963f854be187f5d1b9227003704f03d1
Oleg Lukyanchuk <oleeluk@gmail.com>
2024-07-31 15:14:47 +0300

    Changed cypress.config.js file

---
+++ cypress.config.js
@@ -0,0 +1,22 @@
+const { defineConfig } = require("cypress");
+const createBundler = require("@bahmutov/cypress-esbuild-preprocessor");
+const { addCucumberPreprocessorPlugin } = require("@badeball/cypress-cucumber-preprocessor");
+const createEsbuildPlugin = require("@badeball/cypress-cucumber-preprocessor/esbuild").default;
+
+module.exports = defineConfig({
+  e2e: {
+    specPattern: "**/*.feature",
+    baseUrl: 'https://conduit.realworld.how',
+    projectId: "jpspij",
+    async setupNodeEvents(on, config) {
+      await addCucumberPreprocessorPlugin(on, config);
+      on(
+        "file:preprocessor",
+        createBundler({
+          plugins: [createEsbuildPlugin(config)],
+        })
+      );
+      return config;
+    },
+  },
+});
